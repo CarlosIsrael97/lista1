02f022a1c684b9f8876e806e8c6a1e49e0334ef4
CarlosIsrael97 <60912232+CarlosIsrael97@users.noreply.github.com>
2020-02-26 23:26:58 -0600

    Add files via upload

---
+++ tarea9.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+       int main(){
+       	int a;
+       printf("Introduzca unnunero o letra");
+       	scanf("%d", &a);
+       	
+    switch(a){
+       case 1:
+       printf("\nEs numero\n");
+       break;
+       case 2:
+       printf("\nEs nunero\n");
+       break;
+       case 3:
+       printf("\nEs nunero\n");
+       break;
+       case 4:
+       printf("\nEs numero\n");
+       break;
+       case 5:
+       printf("\nEs numero\n");
+       break;
+       case 6:
+       printf("\nEs numero\n");
+       break;
+       case 7:
+       printf("\nEs numero\n");
+       break;
+       case 8:
+       printf("\nEs numero\n");
+       break;
+       case 9:
+       printf("\nEs numero\n");
+       break;
+       case 0:
+       printf("\nEs numero\n");
+       break;
+     default:
+     printf("es una letra");
+   }
+    
+       	}
